77304183d1493f72f3c5d19e1ab2304c270ae6c3
Peter Richards <peter.jrichards@yahoo.com>
2015-09-09 04:10:16 +0700

    cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-devise_for :users, :path => '', :path_names => {:sign_in => 'im_ron_antill ', :sign_out => 'sign out', :sign_up => "north_remebers69"}
-  get 'static_pages/home'
-  get 'about', to: 'static_pages#about'
+  devise_for :users, :path => '', :path_names => {:sign_in => 'im_ron_antill ', :sign_out => 'sign out', :sign_up => "north_remebers69"}
+  
+  get '/home', to: 'static_pages#home', as: :home
+  get '/about', to: 'static_pages#about', as: :about
+
   resources :posts do
     member do
       put "like" , to: "posts#upvote"
       put "dislike" , to: "posts#downvote"
     end
   end
+  
   root 'posts#index'
 end
