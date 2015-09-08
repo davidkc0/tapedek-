1dd8025eddf5699332377c30830d8985589e26c9
Peter Richards <peter.jrichards@yahoo.com>
2015-09-09 05:34:21 +0700

    User can sign in with username or password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,35 @@
 class User < ActiveRecord::Base
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :username, :presence => true, length: { in: 3..22 }, uniqueness: true
+
+  validates :username, presence: true, length: { in: 4..22 },
+            uniqueness: {case_sensitive: false}
 
   has_many :posts
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
+    end
+  end
+    
 end
