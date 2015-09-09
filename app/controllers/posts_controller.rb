955612152b61ded09d2bf23af5eea2e9c98d5a87
Peter Richards <peter.jrichards@yahoo.com>
2015-09-09 10:33:59 +0700

    working random link

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,11 @@ class PostsController < ApplicationController
   def show
   end
 
+  def random
+    @post = Post.offset(rand(Post.count)).first
+    render :show
+  end
+
   # GET /posts/new
   def new
     @post = Post.new
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   
   get '/home', to: 'static_pages#home', as: :home
   get '/about', to: 'static_pages#about', as: :about
+  get '/random', to: 'posts#random', as: :random
 
   resources :posts do
     member do
