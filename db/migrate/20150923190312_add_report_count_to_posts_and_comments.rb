54a18ed00357179fcfb071f12b27fee0bb836ab0
Peter Richards <peter.jrichards@yahoo.com>
2015-09-24 03:19:29 +0700

    added reports counter cache

--- db/migrate/20150923190312_add_report_count_to_posts_and_comments.rb
+++ db/migrate/20150923190312_add_report_count_to_posts_and_comments.rb
@@ -1,6 +1,6 @@
 class AddReportCountToPostsAndComments < ActiveRecord::Migration
   def change
-    add_column :posts, :report_count, :integer, default: 0
-    add_column :comments, :report_count, :integer, default: 0
+    add_column :posts, :reports_count, :integer, default: 0
+    add_column :comments, :reports_count, :integer, default: 0
   end
 end
