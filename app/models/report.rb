b8f2fae71367ca4f7c6397793ae992a7aadf7fd4
Peter Richards <peter.jrichards@yahoo.com>
2015-09-24 02:49:32 +0700

    validate uniqueness of reporting

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,5 @@
 class Report < ActiveRecord::Base
   belongs_to :reportable, polymorphic: true
   belongs_to :user
+  validates :user_id, uniqueness: { scope: [:reportable_type, :reportable_id] }
 end
