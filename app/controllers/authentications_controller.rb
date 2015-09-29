f7b38a12aa96871c112697f6612909bb0152bd3d
davidkc0 <dvid1994@gmail.com>
2015-09-29 17:58:41 -0400

    styling update

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -3,13 +3,13 @@ class AuthenticationsController < ApplicationController
     omniauth = request.env["omniauth.auth"]
     authentication = Authentication.find_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
     if authentication
-      flash[:notice] = "Signed in successfully."
+      flash[:notice] = "signed in successfully."
       sign_in_and_redirect(:user, authentication.user)
     else
       user = User.new
       user.apply_omniauth(omniauth)
       if user.save
-        flash[:notice] = "Signed in successfully."
+        flash[:notice] = "signed in successfully."
         sign_in_and_redirect(:user, user)
       else
         flash[:notice] = "#{user.errors.full_messages}"
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,7 @@ class MessagesController < ApplicationController
   def create
     recipients = User.where(id: params['recipients'])
     conversation = current_user.send_message(recipients, params[:message][:body], params[:message][:subject]).conversation
-    flash[:success] = "Message has been sent!"
+    flash[:success] = "message has been sent!"
     redirect_to conversation_path(conversation)
   end
 end
