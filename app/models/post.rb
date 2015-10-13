ec4c1c625ef858333dfce3b38ffafdfbce2ff007
davidkc0 <dvid1994@gmail.com>
2015-10-13 17:31:53 -0400

    title limit update

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,7 @@ class Post < ActiveRecord::Base
   acts_as_taggable
 
 	validates :title, presence: true, 
-			length: {minimum: 4, maximum: 55}
+			length: {minimum: 2, maximum: 60}
 
 	validates :video_url, presence: true, uniqueness: true
 
